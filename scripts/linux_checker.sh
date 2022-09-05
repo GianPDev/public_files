8e6e8b36ea101f7341c0d6ea85d6c2b54a8a7c53
GianPDev <gianpdev@gmail.com>
2022-09-05 23:34:36 +1000

    feat: added script to check various groups and commands on POSIX devices

---
+++ scripts/linux_checker.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+echo "### Checking things for user: $(whoami)"
+
+set -- "wheel" "sudo" "docker" "podman" 
+#groups=("wheel" "sudo" "docker" "podman")
+echo "### Checking groups"
+printf %s\,\   "$@"
+echo ""
+for group in "$@"; do
+if [ $(getent group $group) ]; then
+echo "[AVAILABLE✅]\t- $group group"
+else
+echo "[MISSING❌]\t- $group group"
+fi
+done
+
+set -- "sudo" "bash" "nala" "apt" "yum" "dnf" "apk" "snap" "git" "curl" "wget" "make" "nvim" "iptables" "firewall-cmd" "ufw" "docker" "podman" "docker-compose" "podman-compose" "getenforce" "aa-status" "cargo" "node" "nvm" "python" "python3" "pip" "pip3"
+echo "### Checking commands"
+printf %s\,\   "$@"
+echo ""
+for cmd in "$@"; do
+if [ -x "$(command -v $cmd)" ]; then
+echo "[AVAILABLE✅]\t- $cmd"
+else
+echo "[MISSING❌]\t- $cmd"
+fi
+done
